41cf85f5b6014737113082d1266fa366fcc94172
xmtx <xmtx.2015@gmail.com>
2021-05-07 10:39:44 +0800

    dubbo demo 2.7.8 报错

--- dubbo_custom/src/main/java/com/example/custom/demo/controller/CustomController.java
+++ dubbo_custom/src/main/java/com/example/custom/demo/controller/CustomController.java
@@ -2,19 +2,19 @@ package com.example.custom.demo.controller;
 
 import com.example.custom.demo.service.DemoService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/custom")
 public class CustomController {
 
-    @DubboReference
-    private DemoService demoService;
+    @DubboReference(version = "1.0.1")
+    DemoService demoService;
 
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    String hello(String name){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    String hello(String name) {
         return demoService.say(name);
 
     }
